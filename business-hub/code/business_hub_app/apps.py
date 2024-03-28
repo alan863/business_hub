f37440bce90c71f96aac79c24f0acb200f7c7bbf
alan863 <alant-2003@mail.ru>
2024-03-28 10:39:24 +0500

    First commit

---
+++ business-hub/code/business_hub_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class businessHubAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'business_hub_app'
--- tlab_partners/tlab_partners/tlab_partners_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TlabPartnersAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tlab_partners_app'
